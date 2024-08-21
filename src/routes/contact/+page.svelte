9dce5d49d7a81be898eb86e0fa15a4bb7881b592
Joshua Tahanlangit <contact@thisjt.me>
2024-08-21 22:38:54 +0800

    refactor: show map more elegantly

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,27 +1,37 @@
 <script>
 	import PageHeader from '$lib/PageHeader.svelte';
 	import PageTitle from '$lib/PageTitle.svelte';
+	import { onMount } from 'svelte';
+
+	/**@type {HTMLIFrameElement}*/
+	let gmapcanvas;
+
+	onMount(() => {
+		gmapcanvas.addEventListener('load', () => {
+			setTimeout(() => gmapcanvas.classList.remove('opacity-0'), 100);
+		});
+	});
 </script>
 
 <PageTitle text="contact" />
 <div class="px-4 w-full">
 	<PageHeader heading="Contact" />
-	<p class="mb-4 mt-6">
-		I live somewhere here:
+	<p class="mb-4 mt-6">I live somewhere here:</p>
+	<div class="w-full" style="background-color:rgba(0,0,0,0.1)">
 		<iframe
 			style="filter:invert(100%) grayscale(1)"
-			class="w-full"
+			class="w-full transition opacity-0"
 			title="thisjtme Location"
 			height="400"
 			frameborder="0"
 			scrolling="no"
 			marginheight="0"
 			marginwidth="0"
+			bind:this={gmapcanvas}
 			id="gmap_canvas"
 			src="https://maps.google.com/maps?width=976&amp;height=400&amp;hl=en&amp;q=Mandaue%20City%20+(thisjtme%20Location)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
 		></iframe>
-		<script type="text/javascript" src="https://embedmaps.com/google-maps-authorization/script.js?id=070cb1a1e72904e4cdf2d7d362ac7689dfe1710e"></script>
-	</p>
+	</div>
 	<PageHeader heading="How to reach out to me" />
 	<p class="mb-4 text-sm">
 		You can reach out to me using the provided email and social media links. I check my emails at least once a day so that's the best way to reach out to me.
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -4,10 +4,7 @@
 </script>
 
 <PageTitle text="portfolio" />
-<PageHeader heading="Portfolio" />
 <div class="px-4 w-full">
-	<p class="" style="text-justify: distribute;">
-		Hello there! I'm Joshua, a passionate and versatile front-end developer, and I'm thrilled to have you visit my personal website. Here you'll find a digital canvas where I bring
-		ideas to life through lines of code, creating unique and impactful web experiences.
-	</p>
+	<PageHeader heading="Portfolio" />
+	<p class="mb-4">TEST</p>
 </div>
