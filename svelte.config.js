9e1087d17c86555afbe85ac5b1deed7ecdc662b0
Joshua Tahanlangit <contact@thisjt.me>
2024-08-24 12:07:56 +0800

    fix: gh paths config issue

--- svelte.config.js
+++ svelte.config.js
@@ -13,9 +13,6 @@ const config = {
 		adapter: adapter({
 			fallback: '404.html',
 		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
-		},
 	},
 	preprocess: [vitePreprocess()],
 };
