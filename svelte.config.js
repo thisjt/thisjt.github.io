da62a8bdfc079be8f3506053234958cb85e141bb
Joshua Tahanlangit <contact@thisjt.me>
2024-08-31 21:19:44 +0800

    refactor: use vercel adapter
    ci: disable automatic deployment on gh pages
    chore: use vercel adapter
    chore: fix sitemap gen cli

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,9 +10,7 @@ const config = {
 		handler(warning);
 	},
 	kit: {
-		adapter: adapter({
-			fallback: '404.html',
-		}),
+		adapter: adapter(),
 	},
 	preprocess: [vitePreprocess()],
 };
