1ca18f2dde5cfc69cfb8bb2caa409d1e35345b7e
Joshua Tahanlangit <contact@thisjt.me>
2024-09-04 22:33:32 +0800

    feat: install posthog

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,12 @@
+import { browser } from '$app/environment';
+import posthog from 'posthog-js';
+
 export const prerender = true;
+
+export const load = async () => {
+	if (browser)
+		posthog.init('phc_wWIh6uSCIlJrI3SZINvfgEBzy2xeCDQSmgw54mwnMLE', {
+			api_host: 'https://us.i.posthog.com',
+			person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+		});
+};
