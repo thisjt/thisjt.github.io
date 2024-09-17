3cc2b22a73254701bf5eb47823f428d519454fd9
Joshua Tahanlangit <contact@thisjt.me>
2024-09-17 23:16:11 +0800

    Revert "refactor: use vercel adapter"
    This reverts commit da62a8bdfc079be8f3506053234958cb85e141bb.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,9 @@ const config = {
 		handler(warning);
 	},
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html',
+		}),
 	},
 	preprocess: [vitePreprocess()],
 };
